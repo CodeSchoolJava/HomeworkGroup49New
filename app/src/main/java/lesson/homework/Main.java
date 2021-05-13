6b57042e0cdcb72c42f4ce9f4eb92b2a951067ce
Artyom <mtiom12@gmail.com>
2021-05-13 21:25:44 +0400

    --test--

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -3,6 +3,7 @@ package lesson.homework;
 public class Main {
     public static void main(String[] args) {
         //write your code here
+        
         System.out.println("Hello java group 49");
     }
 }
