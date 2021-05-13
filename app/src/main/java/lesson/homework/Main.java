938a49348a94ff08eae531563b1df287121572a7
Razmik <raz.vardanyan@gmail.com>
2021-05-13 20:23:28 +0400

    commit sout

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -2,6 +2,7 @@ package lesson.homework;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("123");
         //write your code here
         System.out.println("Hello java group 49");
     }
