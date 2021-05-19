ef236d29200183e6d6a243b209241ea33f5d04ee
Razmik <raz.vardanyan@gmail.com>
2021-05-19 10:49:18 +0400

    Animal-Homework-50

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,7 +1,33 @@
 package lesson.homework;
 
+import lesson.homework.animal.*;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        Fish d = new Fish();
+        Cat c = new Cat("Fluffy");
+        Animal a = new Fish();
+        Animal e = new Spider();
+        Pet p = new Cat();
+        d.eat();
+        d.walk();
+
+        c.eat();
+        c.play();
+        System.out.println("Cat name : " + c.getName());
+        c.walk();
+
+        a.eat();
+        a.walk();
+
+        e.eat();
+        e.walk();
+
+        p.play();
+        p.setName("Luna");
+        System.out.println("Cat name : " + p.getName());
 
     }
 }
