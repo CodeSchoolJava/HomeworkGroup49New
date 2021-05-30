69a21c1468890833669fdf3ea1f8bd3c78146a3a
samvel.musheghyan <samvel.musheghyan@betconstruct.com>
2021-05-30 14:23:52 +0400

    -commit test

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,12 +1,23 @@
 package lesson.homework;
 
+import lesson.homework.userinfo.Registration;
+import lesson.homework.userinfo.UserInfo;
+
 public class Main {
     public static void main(String[] args) {
-        //write your code here
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Hello java group 49");
+        Registration user = new Registration();
+        user.setName("Walod");
+        user.setSurName("Hamazaszpyan");
 
+        UserInfo userInfo = UserInfo.getInstance();
+
+        try {
+            System.out.println(userInfo.getBirthDay().intern());
+            System.out.println(userInfo.getName());
+            System.out.println(userInfo.getSurName());
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(userInfo.getName());
     }
 }
