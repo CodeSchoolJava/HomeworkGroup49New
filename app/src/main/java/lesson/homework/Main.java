2cf99433a1eeec8b0a0f57b2822e75cee3b8b77d
Razmik <raz.vardanyan@gmail.com>
2021-05-30 14:24:30 +0400

    Singleton-Example

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,7 +1,20 @@
 package lesson.homework;
 
+import lesson.homework.userinfo.Registration;
+import lesson.homework.userinfo.UserInfo;
+
 public class Main {
     public static void main(String[] args) {
+        Registration registration = new Registration();
+        registration.setName("Razmik");
+        registration.setSurname("Vardanyan");
 
+        UserInfo userInfo = UserInfo.getInstance();
+        System.out.println(userInfo.getName() + " " + userInfo.getSurName());
+        try {
+            System.out.println(userInfo.getBirthday().length());
+        } catch (NullPointerException n) {
+            System.out.println("Register your Birthday" + " ::::: " + n);
+        }
     }
 }
---
+++ app/src/main/java/lesson/homework/userinfo/UserInfo.java
@@ -0,0 +1,49 @@
+package lesson.homework.userinfo;
+
+public class UserInfo {
+    private static UserInfo userInfo;
+    private String name;
+    private String surName;
+    private String birthday;
+
+    private UserInfo() {
+    }
+
+    public static UserInfo getInstance() {
+        if (userInfo == null) {
+            userInfo = new UserInfo();
+        }
+        return userInfo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        if (name == null) {
+            return "Write a name";
+        }
+        return name;
+    }
+
+    public String getSurName() {
+        if (surName == null) {
+            return "Write a surName";
+        }
+        return surName;
+    }
+
+    public String getBirthday() throws NullPointerException {
+
+        return birthday;
+    }
+}
