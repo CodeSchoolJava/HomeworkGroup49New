b3b1d2a8e8a4b7ccb1534c507b55d4ae3c0aaac0
armangyulnazaryan <arman.gyulnazaryan@betconstruct.com>
2021-06-04 21:46:18 +0400

    ArrayList example

---
+++ app/src/main/java/lesson/collection/MyList.java
@@ -0,0 +1,6 @@
+package lesson.collection;
+
+public interface MyList<T> {
+    int size();
+
+}
---
+++ app/src/main/java/lesson/collection/arraylist/MyArrayList.java
@@ -0,0 +1,20 @@
+package lesson.collection.arraylist;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import lesson.collection.MyList;
+
+public class MyArrayList <T> implements MyList<T> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
