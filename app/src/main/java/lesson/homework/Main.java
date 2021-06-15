cd2ce7c48a5d65f4cf936431ee582ed3760ab053
aramd <aram.davidyan.39@gmail.com>
2021-06-15 17:16:55 +0400

    arrayList

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,23 +1,48 @@
 package lesson.homework;
 
-import lesson.homework.userInfo.Registration;
-import lesson.homework.userInfo.UserInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+import lesson.homework.collection.MyArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Registration user = new Registration();
-        user.setName("Aram");
-        user.setSurname("Davidyan");
+
+        MyArrayList<String> myArrayList = new MyArrayList<String>();
+        myArrayList.add("avelaci");
+        myArrayList.add("avelaci eli");
+        myArrayList.add("avelaci mi hat el");
+        myArrayList.add("avelaci es mekn el");
+        myArrayList.add("avelaci es el u verj");
+        myArrayList.remove(3);
+        myArrayList.add("esi avelacra nra texy",2);
+        myArrayList.size();
+        myArrayList.isEmpty();
+        System.out.println(myArrayList);
 
 
-        UserInfo userInfo = UserInfo.getInstance();
-        try {
-            System.out.println(userInfo.getBirthDay().intern());
-            System.out.println(userInfo.getName());
-            System.out.println(userInfo.getSurName());
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
+
+
+
+
+
+
+
+
+
+//        Registration user = new Registration();
+//        user.setName("Aram");
+//        user.setSurname("Davidyan");
+//
+//
+//        UserInfo userInfo = UserInfo.getInstance();
+//        try {
+//            System.out.println(userInfo.getBirthDay().intern());
+//            System.out.println(userInfo.getName());
+//            System.out.println(userInfo.getSurName());
+//        } catch (NullPointerException e) {
+//            System.out.println(e.getMessage());
+//        }
 
     }
 }
