3dbd3e1acd67716f55e88c6fb481e67f6d228ee1
Liana0604 <liana1991ant@gmail.com>
2021-06-27 20:00:21 +0400

    Singleton doublecheck

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,5 +1,8 @@
 package lesson.homework;
 
+import lesson.classwork.MyRunable;
+import lesson.classwork.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         //write your code here
@@ -8,5 +11,28 @@ public class Main {
         }
         System.out.println("Hello java group 49");
 
+        MyThread myThread1=new MyThread();
+        MyThread myThread2=new MyThread();
+        Thread thread1=new Thread(new MyRunable());
+        Thread thread2=new Thread(new MyRunable());
+
+        System.out.println("MyThread1");
+        myThread1.start();
+        try {
+            MyThread.sleep(3000);
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("MyThread2");
+        myThread2.start();
+        try {
+            myThread2.wait(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Thread1");
+        thread1.start();
+        System.out.println("Thread2");
+        thread2.start();
     }
 }
