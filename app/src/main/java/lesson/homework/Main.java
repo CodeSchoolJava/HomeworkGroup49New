a85e73f236f8b6f90cae657a49b65430ea9bb35c
aramd <aram.davidyan.39@gmail.com>
2021-06-27 20:01:01 +0400

    double check Singleton

--- app/src/main/java/lesson/homework/Main.java
+++ app/src/main/java/lesson/homework/Main.java
@@ -1,23 +1,56 @@
 package lesson.homework;
 
+import lesson.homework.thread.MyRunnable;
+import lesson.homework.thread.MyThread;
 import lesson.homework.userInfo.Registration;
 import lesson.homework.userInfo.UserInfo;
 
 public class Main {
     public static void main(String[] args) {
-        Registration user = new Registration();
-        user.setName("Aram");
-        user.setSurname("Davidyan");
 
+        MyThread myThread1 = new MyThread();
+        MyThread myThread2 = new MyThread();
+        Thread thread1 = new Thread(new MyRunnable());
+        Thread thread2 = new Thread(new MyRunnable());
 
-        UserInfo userInfo = UserInfo.getInstance();
+        System.out.println("MyThread");
+        myThread1.start();
         try {
-            System.out.println(userInfo.getBirthDay().intern());
-            System.out.println(userInfo.getName());
-            System.out.println(userInfo.getSurName());
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+            MyThread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException" + e.getMessage());
         }
+        System.out.println("MyThread");
+        myThread2.start();
+        System.out.println("Thread");
+        thread1.start();
+        System.out.println("Thread");
+        thread2.start();
+
+
+
+
+
+
+
+
+
+
+
+
+//        Registration user = new Registration();
+//        user.setName("Aram");
+//        user.setSurname("Davidyan");
+//
+//
+//        UserInfo userInfo = UserInfo.getInstance();
+//        try {
+//            System.out.println(userInfo.getBirthDay().intern());
+//            System.out.println(userInfo.getName());
+//            System.out.println(userInfo.getSurName());
+//        } catch (NullPointerException e) {
+//            System.out.println(e.getMessage());
+//        }
 
     }
 }
---
+++ app/src/main/java/lesson/homework/thread/Singleton.java
@@ -0,0 +1,30 @@
+package lesson.homework.thread;
+
+import androidx.annotation.NonNull;
+
+public class Singleton {
+
+    private static volatile Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+
+        }
+        return singleton;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
